48ba535be4e15198e5655a3894104bef04df1806
Ashwini Khare <khare.ashwini@gmail.com>
2015-06-20 14:28:01 -0700

    Added HTTP method to add seat request :confused:

--- android/app/src/main/java/in/ashwinikhare/apslabs/tickets_fragment.java
+++ android/app/src/main/java/in/ashwinikhare/apslabs/tickets_fragment.java
@@ -8,6 +8,12 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Created by ash on 6/20/15.
  */
@@ -52,5 +58,48 @@ public class tickets_fragment extends DialogFragment {
         String name = nameEditText.getText().toString();
         String email = emailEditText.getText().toString();
         String qty = qtyEditText.getText().toString();
+
+        /*
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+
+       new TextHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String res) {
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+        }
+
+        */
+
+        String urlParameters  = "name="+name+"&email="+email+"&qty="+qty;
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
+        String request        = "https://mighty-plateau-5584.herokuapp.com/tickets/feed";
+
+        try {
+            URL url = new URL(request);
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setDoOutput( true );
+            conn.setInstanceFollowRedirects( false );
+            conn.setRequestMethod( "POST" );
+            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty( "charset", "utf-8");
+            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            conn.setUseCaches( false );
+            try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+                wr.write(postData);
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
     }
 }
