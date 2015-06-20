427a928e19e19dccd04f72aa96749d00a4e89f6d
Amanpreet Singh <apsdehal@gmail.com>
2015-06-20 13:10:39 -0700

    Add ticket frontend js

--- public/js/tickets.js
+++ public/js/tickets.js
@@ -10,14 +10,38 @@ Tickets.prototype.start = function () {
 	var isOnTicket = (window.location.pathname === '/tickets');
 
 	if (isOnTicket) {
-		$.getJSON('/tickets/info', function (info) {
-			for (var i = 0; i < info.length; i++) {
-
-			}
+		$.getJSON('/tickets/all', function (info) {
+			handleTickets(info);
 		});
 	}
 };
 
 
-function TicketRow(info) {
+function handleTickets(info) {
+	var html = '<table>';
+	html += '<th>Seat</th>';
+	html += '<th>Name</th>';
+	html += '<th>Quantity</th>';
+	html += '<th>Cost</th>';
+	html += '<th>Total</th>';
+	for(var i = 0; i < info.length; i++) {
+		html += ticketRow(info[i]);
+	}
+	html += '</table>'
+	$('.ticket-div').html(html);
+}
+
+function ticketRow(info) {
+ 	var html = '';
+ 	if (!info.seat || !info.name || !info.quantity || !info.cost || !info.total) {
+ 		return html;
+ 	}
+ 	html += '<tr>'
+ 	html += '<td>' + info.seat + '</td>';
+ 	html += '<td>' + info.name + '</td>';
+ 	html += '<td>' + info.quantity + '</td>';
+ 	html += '<td>' + info.cost + '</td>';
+ 	html += '<td>' + info.total + '</td>';
+ 	html += '</tr>';
+ 	return html;
 }
