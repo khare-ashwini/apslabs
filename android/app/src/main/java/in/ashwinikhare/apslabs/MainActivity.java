16e16682994ceb341744aaf24e8865feb4567134
Ashwini Khare <khare.ashwini@gmail.com>
2015-06-20 01:37:24 -0700

    Material Design :satisfied:

--- android/app/src/main/java/in/ashwinikhare/apslabs/MainActivity.java
+++ android/app/src/main/java/in/ashwinikhare/apslabs/MainActivity.java
@@ -1,17 +1,21 @@
 package in.ashwinikhare.apslabs;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set a ToolBar to replace the ActionBar.
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
     }
 
 
